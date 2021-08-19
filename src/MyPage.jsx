e5cd62f3a6014f889a9da864c644b1d3e1c2acad
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-08-19 10:07:07 -0500

    Empezando clase 77

--- src/MyPage.jsx
+++ src/MyPage.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
+
+const initialTheme = 'light';
 
 const MyPage = () => {
+	const [theme, setTheme] = useState(initialTheme);
+
+	const handleTheme = event => setTheme(event.target.value);
+
 	return (
-		<div>
-			<h2>This is my page</h2>
-			<h2>This is my page</h2>
+		<div className="my-page">
+			<Header theme={theme} handleTheme={handleTheme} />
+			<Main theme={theme} />
+			<Footer theme={theme} />
 		</div>
 	);
 };
