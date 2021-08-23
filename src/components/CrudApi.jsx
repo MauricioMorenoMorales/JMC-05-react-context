31e15db46be260ac598865d1b35ec6ff3abf507c
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-08-23 07:42:49 -0500

    Empezando clase 83

--- src/components/CrudApi.jsx
+++ src/components/CrudApi.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import { helpHttp } from '../helpers/helpHttp';
+import React from 'react';
 
 import CrudForm from './CrudForm';
 import CrudTable from './CrudTable';
@@ -7,95 +6,10 @@ import Loader from './Loader';
 import Message from './Message';
 
 const CrudApi = () => {
-	const [db, setDb] = useState(null),
-		[dataToEdit, setDataToEdit] = useState(null), // Is used in the <form /> and is activated on the component <crudTable />
-		[error, setError] = useState(null),
-		[loading, setLoading] = useState(true);
-
-	const api = helpHttp();
-	let url = 'http://localhost:3333/santos';
-
-	useEffect(() => {
-		setLoading(true);
-		helpHttp()
-			.get(url)
-			.then(res => {
-				if (!res.err) {
-					setDb(res);
-					setError(null);
-				} else {
-					setDb(null);
-					setError(res);
-				}
-			});
-		setLoading(false);
-	}, [url]);
-
-	//Here you have the crud functions create update delete, but not get that is default
-	//Create data inside the component <CrudForm />
-	const createData = data => {
-		data.id = Date.now();
-		const options = {
-			body: data,
-			headers: { 'content-type': 'application/json' },
-		};
-
-		api.post(url, options).then(fetchResponse => {
-			console.log(fetchResponse);
-			if (!fetchResponse.err) {
-				setDb([...db, fetchResponse]);
-			} else {
-				setError(fetchResponse);
-			}
-		});
-
-		setDb([...db, data]);
-	}; //!||
-	const updateData = data => {
-		const endPoint = `${url}/${data.id}`;
-		const options = {
-			body: data,
-			headers: { 'content-type': 'application/json' },
-		};
-		api.put(endPoint, options).then(postResponse => {
-			if (!postResponse.err) {
-				let newData = db.map(element =>
-					element.id === data.id ? data : element,
-				);
-				setDb(newData);
-			} else {
-				setError(postResponse);
-			}
-		});
-	}; //!||
-	const deleteData = id => {
-		let isDelete = window.confirm(
-			`¿Estás seguro de eliminar el registro con el id? ${id}`,
-		);
-		if (isDelete) {
-			const endPoint = `${url}/${id}`;
-			const options = {
-				headers: { 'content-type': 'application/json' },
-			};
-			api.del(endPoint, options).then(deleteResponse => {
-				if (!deleteResponse.err) {
-					const newData = db.filter(element => element.id !== id);
-					setDb(newData);
-				} else {
-					setError(deleteResponse);
-				}
-			});
-			const newData = db.filter(element => element.id !== id);
-			setDb(newData);
-		} else {
-			return;
-		}
-	};
-
 	return (
 		<div>
 			<br />
-			<h2>CRUD API</h2>
+			<h2>CRUD API con ContextAPI</h2>
 			<article className="grid-1-2">
 				<h3>{dataToEdit ? 'Editar' : 'Agregar'}</h3>
 				{/* The crud form create and update the data */}
---
+++ src/contexts/crudContext.js
@@ -0,0 +1,105 @@
+import { createContext, useEffect, useState } from 'react';
+import { helpHttp } from '../helpers/helpHttp';
+
+const CrudContext = createContext();
+
+const crudProvider = ({ children }) => {
+	const [db, setDb] = useState(null),
+		[dataToEdit, setDataToEdit] = useState(null), // Is used in the <form /> and is activated on the component <crudTable />
+		[error, setError] = useState(null),
+		[loading, setLoading] = useState(true);
+
+	const api = helpHttp();
+	let url = 'http://localhost:3333/santos';
+
+	useEffect(() => {
+		setLoading(true);
+		helpHttp()
+			.get(url)
+			.then(res => {
+				if (!res.err) {
+					setDb(res);
+					setError(null);
+				} else {
+					setDb(null);
+					setError(res);
+				}
+			});
+		setLoading(false);
+	}, [url]);
+
+	//Here you have the crud functions create update delete, but not get that is default
+	//Create data inside the component <CrudForm />
+	const createData = data => {
+		data.id = Date.now();
+		const options = {
+			body: data,
+			headers: { 'content-type': 'application/json' },
+		};
+
+		api.post(url, options).then(fetchResponse => {
+			console.log(fetchResponse);
+			if (!fetchResponse.err) {
+				setDb([...db, fetchResponse]);
+			} else {
+				setError(fetchResponse);
+			}
+		});
+
+		setDb([...db, data]);
+	}; //!||
+	const updateData = data => {
+		const endPoint = `${url}/${data.id}`;
+		const options = {
+			body: data,
+			headers: { 'content-type': 'application/json' },
+		};
+		api.put(endPoint, options).then(postResponse => {
+			if (!postResponse.err) {
+				let newData = db.map(element =>
+					element.id === data.id ? data : element,
+				);
+				setDb(newData);
+			} else {
+				setError(postResponse);
+			}
+		});
+	}; //!||
+	const deleteData = id => {
+		let isDelete = window.confirm(
+			`¿Estás seguro de eliminar el registro con el id? ${id}`,
+		);
+		if (isDelete) {
+			const endPoint = `${url}/${id}`;
+			const options = {
+				headers: { 'content-type': 'application/json' },
+			};
+			api.del(endPoint, options).then(deleteResponse => {
+				if (!deleteResponse.err) {
+					const newData = db.filter(element => element.id !== id);
+					setDb(newData);
+				} else {
+					setError(deleteResponse);
+				}
+			});
+			const newData = db.filter(element => element.id !== id);
+			setDb(newData);
+		} else {
+			return;
+		}
+	};
+	const data = {
+		createData,
+		updateData,
+		dataToEdit,
+		setDataToEdit,
+		loading,
+		error,
+		db,
+		deleteData,
+	};
+	return <CrudContext.provider value={data}>{children}</CrudContext.provider>;
+};
+
+export { crudProvider };
+export default CrudContext;
